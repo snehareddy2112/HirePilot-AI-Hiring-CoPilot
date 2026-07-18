324ab6b0521a994a14a759cbd8e87d675b7e280a
Sneha Aelijerla <155059049+snehareddy2112@users.noreply.github.com>
2026-07-18 12:45:24 +0530

    Refactor OAuth client initialization and remove logs

--- backend/src/config/google.ts
+++ backend/src/config/google.ts
@@ -3,18 +3,16 @@ dotenv.config();
 
 import { google } from "googleapis";
 
-console.log(
-  process.env.GOOGLE_CLIENT_ID
-);
+export const oauth2Client =
+  new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    `${process.env.BACKEND_URL}/api/auth/google/callback`
+  );
 
 export const mailboxOAuthClient =
   new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID!,
-    process.env.GOOGLE_CLIENT_SECRET!,
-    //"http://localhost:5000/api/mailbox/google/callback"
-     `${process.env.NEXT_PUBLIC_API_URL}/api/mailbox/google/callback`
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    `${process.env.BACKEND_URL}/api/mailbox/google/callback`
   );
-
-console.log(
-    mailboxOAuthClient
-);
